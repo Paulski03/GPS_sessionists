e6aa4b97c31bfbd175e3428f74a9830eb2898305
Michael Paul Solis <michaelpaulsolis@yahoo.com>
2017-07-29 22:40:42 +0800

    Added plan model and database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729114458) do
+ActiveRecord::Schema.define(version: 20170729140132) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "mobile"
     t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
