4b04e18e0936b4f1d824e1a05f65e5fc8a421381
Michael Paul Solis <michaelpaulsolis@yahoo.com>
2017-07-30 22:34:59 +0800

    Added plan model anddb table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729144811) do
+ActiveRecord::Schema.define(version: 20170730132659) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170729144811) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "plan_id"
+    t.string "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
